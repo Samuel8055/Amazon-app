754b36cd38782359dfeb1d6537e2b5bdea585567
Samuel8055 <samueltschweitzer@gmail.com>
2022-05-19 16:59:03 +0530

    Create express server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,18 @@
-console.log('Server code here...')
+import express from 'express';
+import data from './data.js';
+
+const app = express();
+
+app.get('/api/products', (req, res) => {
+  res.json(data.products);
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello!')
+})
+
+const port = process.env.PORT || 5001;
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`)
+})
